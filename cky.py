38a8eacfece5e38ef339203a4c69bd6c917b7084
Pranoy Kovuri <yonarp@Pranoys-MacBook-Pro.local>
2017-11-26 16:16:44 -0600

    Preprocessing Done

--- cky.py
+++ cky.py
@@ -1,6 +1,7 @@
 
 
 import sys
+import numpy as np
 
 
 #The simple logic to write the program is pi[i, j,X] = maximum probability of a constituent with non-terminal X spanning words i . . . j inclusive
@@ -8,30 +9,116 @@ import sys
 ### Base case is to define all words if they are present in the lexicons
 #### The import one, the recursive definition pi(i,j,X) = max(q(x->yz)*pi(i,t,y)*pi(t+1,j,z)) given x->yz is in the grammar and t belongs to i:j-1
 
+class Unary_Rule:
+    def __init__(self,*rule):
+        self.left, self.right = rule[0],rule[1]
+        self.prob = rule[2]
+
+class Binary_Rule:
+    def __init__(self,*rule):
+        self.left, self.right1, self.right2 = rule[0],rule[1],rule[2]
+        self.prob = rule[3]
+class Lexicon:
+    def __init__(self,*rule):
+        self.left, self.word = rule[0],rule[1]
+        self.prob = rule[2]
+
+def preProcessGrammar(grammar_rules):
+    """
+    Preprocessing grammar_rules into unary and binaries.
+    """
+
+    Unaries = []
+    Binaries = []
+    Lexicons = []
+    for i, rule in grammar_rules.items():
+        temp = rule.split()
+        # print("debug: Pranoy")
+        if(len(temp) == 3):
+            if(temp[1][0] >= 'a' and temp[1][0] <= 'z'):
+                lexicon = Lexicon(*temp)
+                Lexicons.append(lexicon)
+            else:
+                print(temp[2])
+                unary = Unary_Rule(*temp)
+                Unaries.append(unary)
+        else:
+            binary = Binary_Rule(*temp)
+            Binaries.append(binary)
+
+    return Binaries, Unaries, Lexicons
+
+def getNonTerminals(Binaries,Unaries,Lexicons):
+    N = set()
+    for rule in Unaries:
+        N.add(rule.left)
+        N.add(rule.right)
+    for rule in Binaries:
+        N.add(rule.left)
+        N.add(rule.right1)
+        N.add(rule.right2)
+    for rule in Lexicons:
+        N.add(rule.left)
+
+    return list(N)
+
 
 def ckyAlgorithm(sentence, grammar_rules):
-	
+    """
+    inputs to our algorithm
+    sentence: x1,x2, ....  xn
+    PCFG: G = [N,Sigma,S,R,q]
+    N: set of non-terminals
+    Sigma: set of terminals
+    S: start symbol of the grammar
+    R: Set of Rules
+
+    Algorithm: 
+
+    Loop: iterate over the span: span varies from 1 to n-1
+        Loop: iterate over i, i varies from 1 to n-l,  here l is the span variable
+            set j = i + l, since j is the span limit
+                Loop: for all non-terminal N setting them as X
+                    pi(i,j,X) = max(q(X->YZ)*pi(i,t,Y)*pi(t+1,j,Z)) ,, t belongs to i:j-1
+                    and Q(X->YZ) is in the grammar
+
+    returns [mostProbableParse, probability]
+    """
+    Binaries, Unaries, Lexicons = preProcessGrammar(grammar_rules)
+    N = getNonTerminals(Binaries,Unaries,Lexicons)
+    length = len(sentence.split())
+    words = sentence.split()
+    score = [[[0 for i in range(len(N))] for j in range(length)] for k in range(length)]
+    back = [[[[] for i in range(len(N))] for j in range(length)] for k in range(length)]
+
+    # print("debug: score.shape", np.array(score).shape)
+
+    ## For Unaries 
+    # for i in range(length):
+
 
 
 def main():
-	grammar_rules = {}
-	sents = []
-	print("Debug ", sys.argv)
+    grammar_rules = {}
+    sents = []
+    # print("debug: Grammar Rules :::: \n")
 
-	with open(sys.argv[1]) as file:
-		for i,line in enumerate(file):
-			grammar_rules[i] = line
-			
+    with open(sys.argv[1]) as file:
+        for i,line in enumerate(file):
+            grammar_rules[i] = line
+            # print("debug: Grammar Rules",i,":",line)
+            
+    # print("\debug: n\nSentences :::: \n")
+    with open(sys.argv[2]) as file:
+        for i,line in enumerate(file):
+            sents.append(line)
+            # print("debug: Sentence",i,":",line)
 
-	with open(sys.argv[2]) as file:
-		for line in file:
-			sents.append(line)
 
-
-	for sent in sents:
-		print("PROCESSING SENTENCE:",sent,'\n')
-		ckyAlgorithm(sentence, grammar_rules)
+    for sent in sents:
+        print("PROCESSING SENTENCE:",sent,'\n')
+        ckyAlgorithm(sent, grammar_rules)
 ## Dynamic Programming Task 
 # How will you do it!!!??
 
-main()
+# main()
